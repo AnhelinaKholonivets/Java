bdc7a8a12ed5c13cb685fd94769832f5fbc61e71
Lina <caprice.angelok@gmail.com>
2021-09-16 15:38:27 +0300

    Shape

---
+++ Shape/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Shape shape = new Rectangle(new Point(3,4),new Point(7,6));
+        System.out.println(shape);
+    }
+}
